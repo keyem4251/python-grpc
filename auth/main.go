cca289e6a249e3ba8a5e74516468a25d1bdcb45d
keyem4251 <kisk.emura@gmail.com>
2021-04-24 15:11:40 +0900

    go grpc server greeter

--- auth/main.go
+++ auth/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	pb "keyem4251/try-grpc-microservice-auth/pb"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50052"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloResponse{Message: "Hello " + in.GetName()}, nil
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- auth/pb_gen.sh
+++ auth/pb_gen.sh
@@ -2,4 +2,4 @@
 
 protoc -I ../protobufs --go_out=pb/ --go_opt=paths=source_relative \
     --go-grpc_out=pb/ --go-grpc_opt=paths=source_relative \
-    ../protobufs/recommendations.proto
+    ../protobufs/auth.proto
