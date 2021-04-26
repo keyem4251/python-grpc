6d1e40e7a9a4f2f2644ce6b371eeb7466629a463
keyem4251 <kisk.emura@gmail.com>
2021-04-26 22:44:38 +0900

    firebase config from env

--- marketplace/auth.py
+++ marketplace/auth.py
@@ -1,12 +1,24 @@
 import os
-import json
 import pyrebase
 
-FIREBASE_CONFIG_FILE = os.getenv("FIREBASE_CONFIG_FILE", None)
+  API_KEY = os.getenv("API_KEY")
+  AUTH_DOMAIN = os.getenv("API_KEY")
+  PROJECT_ID = os.getenv("API_KEY")
+  STORAGE_BUCKET = os.getenv("API_KEY")
+  MESSAGING_SENDER_ID = os.getenv("API_KEY")
+  APP_ID = os.getenv("API_KEY")
+  MEASUREMENT_ID = os.getenv("API_KEY")
 
 
 def init_auth():
-    with open(FIREBASE_CONFIG_FILE) as f:
-        firebase_config = json.loads(f.read())
+    firebase_config = {
+        "apiKey": API_KEY,
+        "authDoamin": AUTH_DOMAIN,
+        "projectId": PROJECT_ID,
+        "storageBucket": STORAGE_BUCKET,
+        "messagingSenderId": MESSAGING_SENDER_ID,
+        "appId": APP_ID,
+        "measurementId": MEASUREMENT_ID,
+    }
     firebase = pyrebase.initialize_app(firebase_config)
     return firebase.auth()
